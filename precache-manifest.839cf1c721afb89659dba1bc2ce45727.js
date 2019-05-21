7881047abebead469abb19e197385fb67c26b4d4
Kritika Dusad <kritikadusad@gmail.com>
2019-05-21 14:05:38 -0700

    Updates

---
+++ precache-manifest.839cf1c721afb89659dba1bc2ce45727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b8048a6cc8c9d0ee2dc9a30b0671b4",
+    "url": "/NewsFilter-web-app/index.html"
+  },
+  {
+    "revision": "ef49aa8288d5df117436",
+    "url": "/NewsFilter-web-app/static/css/2.4f642d5c.chunk.css"
+  },
+  {
+    "revision": "370160d1dd80c9d58d4a",
+    "url": "/NewsFilter-web-app/static/css/main.8b418096.chunk.css"
+  },
+  {
+    "revision": "ef49aa8288d5df117436",
+    "url": "/NewsFilter-web-app/static/js/2.679f0372.chunk.js"
+  },
+  {
+    "revision": "370160d1dd80c9d58d4a",
+    "url": "/NewsFilter-web-app/static/js/main.ef780fd5.chunk.js"
+  },
+  {
+    "revision": "4638270b5f82ece7f219",
+    "url": "/NewsFilter-web-app/static/js/runtime~main.4cf2687a.js"
+  },
+  {
+    "revision": "0e4c44a39acdd86a4500dfdba35c1464",
+    "url": "/NewsFilter-web-app/static/media/news.0e4c44a3.png"
+  }
+]);
